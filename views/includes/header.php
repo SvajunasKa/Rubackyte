a6e3704c3ed5bb0d58de0a9e4475c3a72600ba30
Svajunas <svajunas.kaseta@gmail.com>
2017-12-10 23:13:12 +0200

    fix

--- views/includes/header.php
+++ views/includes/header.php
@@ -87,7 +87,7 @@
                         <div class="menu-item"><a <?php if(strpos($uri, 'galerija') !== false) { echo 'class="active_bottom"'; } ?> href="galerija.php#muza"><?php echo $meniu->galerijaLt; ?></a></div>
                         <div class="menu-item1"><a <?php if(strpos($uri, 'media') !== false) { echo 'class="active_bottom"'; } ?> href="#"><?php echo $meniu->mediaLt; ?></a>
                             <div class="show-menu">
-                                <div class="drop-list"> <a <?php if(strpos($uri, 'media') !== false) { echo 'class="active_bottom"'; } ?> href="kontaktai.php#muza"><?php echo $meniu->subMenu1Lt; ?></a></div>
+                                 <div class="drop-list"> <a <?php if(strpos($uri, 'media') !== false) { echo 'class="active_bottom"'; } ?> href="kontaktai.php#muza"><?php echo $meniu->subMenu1Lt; ?></a></div>
                                 <div class="drop-list"> <a <?php if(strpos($uri, 'media') !== false) { echo 'class="active_bottom"'; } ?> href="kontaktai.php#muza"><?php echo $meniu->subMenu2Lt; ?></a></div>
                                 <div class="drop-list"> <a <?php if(strpos($uri, 'media') !== false) { echo 'class="active_bottom"'; } ?> href="kontaktai.php#muza"><?php echo $meniu->subMenu3Lt; ?></a></div>
                             </div>
